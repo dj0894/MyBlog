69e351b5fc89f15b5486a848d1bd7faf497a01d2
Deepika Jha <Deepikajha0894@gmail.com>
2021-09-01 22:37:59 -0700

    1.Refactored code to remove redundancy from Api calls in server.js

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ with out nodemon  run using below command
  npx babel-node src/server.js
 for reruning command 
 npx nodemon --exec babel-node src/server.js
-npm install --save mongodb // 
+npm install --save mongodb //allow us to modify our db inside express server code
+
+To call backend from front end used inbuilt fetch Api
--- myBlogBackEnd/src/server.js
+++ myBlogBackEnd/src/server.js
@@ -5,31 +5,31 @@ const PORT = 5000;
 const app = express();
 app.use(bodyParser.json());// Parses json object includes in post request
 
-
-
-app.get('/api/articles/:name', async (req, res) => {
+const withDB = async (operations) => {
     try {
-        const articleName = req.params.name;
+
         const client = await MongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true }) //async call for connecting mongoDB
         const db = client.db('my-blog') // connect db my-blog
-        const articleInfo = await db.collection('articles').findOne({ name: articleName });
-        res.status(200).json(articleInfo);
-
+        await operations(db);
         client.close();
     } catch (error) {
         res.status(500).json({ message: 'Error while connecting database', error })
     }
+}
+
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+    }, res)
+
 
 })
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
-    try {
-
+    withDB(async (db) => {
         const articleName = req.params.name;
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-
-        const db = client.db('my-blog');
-
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         await db.collection('articles').updateOne({ name: articleName }, {
             '$set': {
@@ -37,20 +37,25 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
             },
         });
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
-        console.log(updatedArticleInfo);
         res.status(200).json(updatedArticleInfo);
-
-        client.close();
-    } catch (error) {
-        res.status(500).json({ message: 'Error connecting to db', error });
-    }
+    }, res)
 })
 
 app.post('/api/articles/:name/addComment', async (req, res) => {
     const articleName = req.params.name;
     const { username, text } = req.body;
-    articleInfo[articleName].comments.push({ username, text });
-    res.status(200).send(articleInfo[articleName]);
+    withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                comments: articleInfo.comments.concat({ username, text }),
+            }
+        })
+
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        res.status(200).json(updatedArticleInfo);
+    }, res)
 })
 
 app.listen(PORT, () => console.log('listening on port `${PORT}`'));
